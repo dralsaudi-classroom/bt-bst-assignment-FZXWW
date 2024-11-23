512d95fb5ab449b04c34703bf0728c0c2822e16b
Mohammad <dr.fmfm2004@gmail.com>
2024-11-23 21:37:26 +0300

    bst

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -7,5 +7,7 @@ public class TreeUser {
         // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
         // input: None. results: if the current node of the binary tree is a leaf then flag is set to true otherwise it is set to false. output: flag.
         // you can't call countLeaves from BT.java
+        
+        
     }
 }
