0aa9442f0b65910a296420854477d27b0360acce
Mohmmad Alqurashi <dr.fmfm2004@gmail.com>
2024-11-25 01:49:37 +0300

    Update TreeUser.java

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -1,13 +1,24 @@
 package com.example.project;
 
 public class TreeUser {
-    public static <T> int countLeaves(BT<T> bt) {
-        throw new UnsupportedOperationException("Not supported yet.");
-		// Write the method countLeafs that should return the number of leaf nodes in the tree. A leaf node is a node that has no children.
-        // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
-        // input: None. results: if the current node of the binary tree is a leaf then flag is set to true otherwise it is set to false. output: flag.
-        // you can't call countLeaves from BT.java
+       public static <T> int countLeaves(BT<T> bt) {
+            //throw new UnsupportedOperationException("Not supported yet.");
+    		// Write the method countLeafs that should return the number of leaf nodes in the tree. A leaf node is a node that has no children.
+            // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
+            // input: None. results: if the current node of the binary tree is a leaf then flag is set to true otherwise it is set to false. output: flag.
+            // you can't call countLeaves from BT.java   
+            BTNode<T> root = bt.root;
+            return countLeavesHelper(root);
+        }
         
         
-    }
+        
+        private static <T> int countLeavesHelper(BTNode<T> p) {
+        	if(p == null) return 0;
+        	
+        	if(p.left == null && p.right==null) return 1;
+        	
+        	return countLeavesHelper(p.left) + countLeavesHelper(p.right);
+        	
+        }
 }
